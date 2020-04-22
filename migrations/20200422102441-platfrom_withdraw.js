b6983fe9017d33f22aa096e668e04586e344e374
Rodion <rodion_leginkov@mail.ru>
2020-04-22 13:42:49 +0300

    New migration

---
+++ migrations/20200422102441-platfrom_withdraw.js
@@ -0,0 +1,17 @@
+'use strict';
+
+'use strict';
+
+  module.exports = {
+    up: (queryInterface, Sequelize) => [
+      queryInterface.addColumn('milestones', 'platform', {
+        type: Sequelize.STRING(64),
+      }),
+      queryInterface.addColumn('milestones', 'withdraw', {
+        type: Sequelize.STRING(64),
+      })
+  
+    ],
+  };
+
+ 
