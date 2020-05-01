99b9424a9c996be52d01c48e51d8086299dc352a
Rodion <rodion_leginkov@mail.ru>
2020-05-01 18:08:38 +0300

    Fix

--- src/controllers/auth/forgotPass.post.js
+++ src/controllers/auth/forgotPass.post.js
@@ -53,7 +53,6 @@ router.post('/forgotPass',
           transporter.sendMail(mailOptions, (err, response) =>{
             if (err) throw errors.InternalServerError('Email wasn\`t sent');
             else {
-              console.log('here is the res: ', response);
               res.status(200).json('recovery email sent');
             }
       });
