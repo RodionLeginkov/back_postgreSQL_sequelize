1bccd4f4a08cec315587a32dd3455d10113e3be2
Rodion <rodion_leginkov@mail.ru>
2020-07-01 15:09:11 +0300

    Fix server-ping

--- src/jobs/server-ping.js
+++ src/jobs/server-ping.js
@@ -1,7 +1,7 @@
 const cron = require('node-cron');
 const axios = require('axios');
 module.exports = () => {
-    cron.schedule(' 10 8-23 * * 0-7', async () => {
+    cron.schedule(' */10 8-23 * * 0-7', async () => {
         try {
           await axios.get(`${process.env.PING_FRONTEND_URL}`);
           await axios.get(`${process.env.PING_BACKEND_URL}`);
