5a000217bc508ddbf00b7dec123551f333848746
Kanonir161 <vereshagin.n00@gmail.com>
2020-09-10 14:06:26 +0300

    Fix cron ping message

--- src/jobs/server-ping.js
+++ src/jobs/server-ping.js
@@ -5,12 +5,12 @@ module.exports = () => {
       try {
         await axios.get(`${process.env.PING_BACKEND_URL}`);
        } catch (err) {
-         console.log('Error back', err);
+         console.log('Error back');
        }
        try {
         await axios.get(`${process.env.PING_FRONTEND_URL}`);
        } catch (err) {
-        console.log('Error front', err);
+        console.log('Error front');
        }
     }, {
         scheduled: true,
--- src/server.js
+++ src/server.js
@@ -56,9 +56,12 @@ async function useControllers() {
     const paths = klawSync(`${__dirname}/controllers`, {nodir: true});
     let controllersCount = 0;
     paths.forEach((file) => {
-        if (path.basename(file.path)[0] === '_' || path.basename(file.path)[0] === '.') return;
-        app.use('/', require(file.path));
-        controllersCount++;
+        if (
+			path.basename(file.path)[0] === '_'
+      || path.basename(file.path)[0] === '.'
+		) return;
+		app.use('/', require(file.path));
+		controllersCount++;
     });
     
     console.info(`Total controllers: ${controllersCount}`);
